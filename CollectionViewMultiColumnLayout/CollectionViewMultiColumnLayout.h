d091a514ac588278ffa87c0bf7a5e36e9d3ccb4b
Vincent Esche <regexident@gmail.com>
2017-05-18 12:58:36 +0200

    Initial Commit

---
+++ CollectionViewMultiColumnLayout/CollectionViewMultiColumnLayout.h
@@ -0,0 +1,19 @@
+//
+//  CollectionViewMultiColumnLayout.h
+//  CollectionViewMultiColumnLayout
+//
+//  Created by Vincent Esche on 5/18/17.
+//  Copyright © 2017 Vincent Esche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionViewMultiColumnLayout.
+FOUNDATION_EXPORT double CollectionViewMultiColumnLayoutVersionNumber;
+
+//! Project version string for CollectionViewMultiColumnLayout.
+FOUNDATION_EXPORT const unsigned char CollectionViewMultiColumnLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionViewMultiColumnLayout/PublicHeader.h>
+
+
